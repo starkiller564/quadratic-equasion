6331fd0ba0a28061a07493280d9754c3f041f0c7
Tony <starkiller190293@gmail.com>
2018-01-16 23:23:41 +0300

    roots works always now

--- Project2/Source.cpp
+++ Project2/Source.cpp
@@ -3,13 +3,18 @@ using namespace std;
 
 void output(int, int, int);
 int discriminant(int, int, int);
-void roots(int, int, int, int);
+void roots(double, double, double, double);
 
 void main(void)
 {
 	int a, b, c, d;
 	cout << "Enter numbers for quadratic equation (a, b, c): " << endl;
 	cin >> a >> b >> c;
+	if (a == 0)
+	{
+		cout << "a = 0, enter another number for a: " << endl;
+		cin >> a;
+	}
 
 	output(a, b, c);
 	d = discriminant(a, b, c);
@@ -27,20 +32,27 @@ int discriminant(int a, int b, int c)
 {
 	int D;
 	D = (b * b) - (4 * a * c);
+	cout << "D = " << D << endl;
 	return D;
 }
 
-void roots(int a, int b, int c, int d)
+void roots(double a, double b, double c, double d)
 {
+	double x1, x2;
+	x1 = (-b + sqrt(d)) / (2 * a);
+	x2 = (-b - sqrt(d)) / (2 * a);
+
 	if (d > 0)
 	{
-		cout << "x1 = " << double (-b + sqrt(d)) / (2 * a) << endl;
-		cout << "x2 = " << double (-b - sqrt(d)) / (2 * a) << endl;
+		cout << "x1 = " << x1 << endl;
+		cout << "x2 = " << x2 << endl;
 	}
+
 	if (d == 0)
 	{
-		cout << "x = " << double (-b / (2 * a)) << endl;
+		cout << "x = " << x1 << endl;
 	}
+
 	if (d < 0)
 	{
 		cout << "No roots" << endl;
